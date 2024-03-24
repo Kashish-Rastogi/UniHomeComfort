0e5f247ad0b7523a73c55ad4759cb14707528802
mac-haseeb <ahmad99@uwindsor.ca>
2024-03-24 13:36:51 -0400

    Forget password working

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -283,29 +283,24 @@ class PropertySearchForm(forms.ModelForm):
 
 
 
-##################### Haseeb #################
-        
+##################### Haseeb #####################
 
-# Creating a form for handling password reset
+
+from django import forms
+
 class ForgetPasswordForm(forms.Form):
-    # Defining form fields
     username = forms.CharField(label='Username', max_length=150, widget=forms.TextInput(attrs={'class': 'form-control'}))
     new_password = forms.CharField(label='New Password', widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control'}))
     confirm_password = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control'}))
 
-    # Method to clean and validate form data
     def clean(self):
-        # Call the parent class' clean method to ensure all validations are performed
         cleaned_data = super().clean()
-        # Retrieve cleaned data for new password and confirm password fields
         new_password = cleaned_data.get("new_password")
         confirm_password = cleaned_data.get("confirm_password")
-        # Check if both passwords are provided and match
         if new_password and confirm_password and new_password != confirm_password:
-            # Raise a validation error if passwords don't match
             raise forms.ValidationError("New passwords do not match")
-        # Return the cleaned data
         return cleaned_data
+
 
 
 
